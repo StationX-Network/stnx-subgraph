5525edcbc75f1d4e3bec9fc4e6dda7a3e44befaf
Ankit Dwij <ankitdwiz010@gmail.com>
2024-12-11 13:12:03 +0530

    changes(v2)

--- src/updateRaise.ts
+++ src/updateRaise.ts
@@ -1,42 +1,43 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 
-import { Station } from "../generated/schema"
+import { Station } from "../generated/schema";
 import {
   UpdatePricePerToken as UpdatePricePerTokenEvent,
   UpdateDistributionAmount as UpdateDistributionAmountEvent,
-} from "../generated/StnxEmitter/StnxEmitter"
+} from "../generated/StnxEmitter/StnxEmitter";
 
 export function updatePricePerToken(event: UpdatePricePerTokenEvent): void {
-  let daoAddress = event.params._daoAddress.toHex()
-  let station = new Station(daoAddress)
-  if (!station) return
+  let daoAddress = event.params._daoAddress.toHex();
+  let station = Station.load(daoAddress);
+  if (!station) return;
 
-  station.pricePerToken = event.params._amount
-  station.save()
+  station.pricePerToken = event.params._amount;
+  station.save();
 }
 
 export function updateDistributionAmount(
   event: UpdateDistributionAmountEvent
 ): void {
-  let daoAddress = event.params._daoAddress.toHex()
-  let station = Station.load(daoAddress)
+  let daoAddress = event.params._daoAddress.toHex();
+  let station = Station.load(daoAddress);
 
   // Create a new Station if it doesn't exist
   if (!station) {
-    station = new Station(daoAddress)
+    return;
+    // station = new Station(daoAddress);
     // Initialize any default properties here
   }
 
   // Ensure that pricePerToken and other properties are initialized and not null
   if (!station.pricePerToken) {
-    station.pricePerToken = BigInt.fromI32(1) // Or some default value
+    station.pricePerToken = BigInt.fromI32(1); // Or some default value
   }
 
-  station.distributionAmount = event.params._amount
+  station.distributionAmount = event.params._amount;
   station.raiseAmount = event.params._amount
     .times(station.pricePerToken)
-    .div(BigInt.fromI32(10).pow(18))
+    .div(BigInt.fromI32(10).pow(18));
 
   // Save the updated entity
-  station.save()
+  station.save();
 }
